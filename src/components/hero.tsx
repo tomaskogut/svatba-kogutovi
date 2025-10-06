230073aec9cb1c63c29e65c632584fc1a5dbc410
Tomáš Kogut <kogut.tomas@seznam.cz>
2025-10-06 19:06:34 +0200

    change of import

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,9 @@
 import './hero.css'
+import backgroundimg from '../assets/background.png'
 
 export default function Hero() {
   return (
-    <section className="hero">
+    <section className="hero" style={{ backgroundImage: `url(${backgroundimg})` }}>
       <div className="hero-content">
         <h1 className="hero-title">Tomáš & Markéta</h1>
         <p className="hero-date">2.5.2026, Hodslavice</p>
